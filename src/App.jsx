04f5f1f42e97d8bd49456cf602c4de2e97b478b3
Tai Vo <tantaivo74@gmail.com>
2023-03-03 17:02:26 +0700

    feat: test github

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + Reactaaa</h1>
       <div className="card">
         <button type="button" onClick={() => setCount((prevCount) => prevCount + 1)}>
           count is
