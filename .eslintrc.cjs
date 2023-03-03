8d0fb92318dc7c5b33d0f71c34917d89b8cb74e3
Tai Vo <tantaivo74@gmail.com>
2023-03-03 14:13:13 +0700

    chore: config eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    'react'
+  ],
+  rules: {
+    semi: ['error', 'never'],
+    'comma-dangle': ['error', 'never'],
+    quotes: ['error', 'single'],
+    'import/no-cycle': 0,
+    'react/prop-types': 0,
+    'max-len': ['error', { code: 150 }],
+    'import/prefer-default-export': ['off', { target: 'any' }],
+    'default-param-last': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/destructuring-assignment': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'linebreak-style': 0
+  }
+}
