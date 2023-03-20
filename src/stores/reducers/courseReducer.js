65432d9dac90457a5a7982555573c27e8e4d38d7
Thanh Phong <tangthanh.phong99@gmail.com>
2023-03-20 15:58:54 +0700

    fix(courseReducer): change name type

--- src/stores/reducers/courseReducer.js
+++ src/stores/reducers/courseReducer.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-unused-vars */
 import {
-  SET_CATEGORY, SET_DETAIL_COURSE, GET_COURSES, SET_LIST_COURSE_BY_CATE, SET_LIST_COURSE_PAGINATION
+  GET_CATEGORY, GET_COURSES, GET_DETAIL_COURSE, GET_LIST_COURSE_BY_CATE, GET_LIST_COURSE_PAGINATION
 } from 'stores/types'
 import { failureAction, requestAction, successAction } from 'utils'
 
@@ -8,11 +9,21 @@ const courses = {
   data: [],
   error: null
 }
-// const detailCourse = {
-//   isLoading: false,
-//   data:{},
-//   error: null
-// }
+const detailCourse = {
+  isLoading: false,
+  data: {},
+  error: null
+}
+const listCourseWithPagination = {
+  isLoading: false,
+  data: {},
+  error: null
+}
+const listCourseByCate = {
+  isLoading: false,
+  data: [],
+  error: null
+}
 const initialState = {
   categoryArr: [],
   courses: { ...courses },
@@ -24,7 +35,7 @@ const initialState = {
 
 export const courseReducer = (state = initialState, { type, payload, error }) => {
   switch (type) {
-    case SET_CATEGORY:
+    case GET_CATEGORY:
       return { ...state, categoryArr: payload }
 
     // For get courses
@@ -46,13 +57,13 @@ export const courseReducer = (state = initialState, { type, payload, error }) =>
         courses: { ...state.courses, isLoading: false, error }
       }
     // For get course detail
-    case SET_DETAIL_COURSE:
+    case GET_DETAIL_COURSE:
       return { ...state, detailCourse: payload }
     // For get course with pag
-    case SET_LIST_COURSE_PAGINATION:
+    case GET_LIST_COURSE_PAGINATION:
       return { ...state, listCourseWithPagination: payload }
     // For get course by categories
-    case SET_LIST_COURSE_BY_CATE:
+    case GET_LIST_COURSE_BY_CATE:
       return { ...state, listCourseByCate: payload }
 
     default:
