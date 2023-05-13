8f1f614ebf90300fa20b308a3f380b7722aa0429
Thanh Phong <tangthanh.phong99@gmail.com>
2023-05-13 16:45:57 +0700

    feat: build homeprotectedroute

---
+++ src/components/RoutesControl/HomeProtectedRoute/index.jsx
@@ -0,0 +1,28 @@
+import { Footer, Header } from 'components'
+import { ROUTES_NAME } from 'constant'
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Redirect, Route } from 'react-router'
+
+function HomeProtectedRoute({ component: Component, path, ...rest }) {
+  const { authenticated } = useSelector((state) => state.userReducer)
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => (
+        authenticated
+          ? (
+            <>
+              <Header />
+              <Component {...props} />
+              <Footer />
+            </>
+          )
+          : <Redirect to={{ pathname: ROUTES_NAME.LOGIN }} />
+      )}
+    />
+  )
+}
+
+export default HomeProtectedRoute
