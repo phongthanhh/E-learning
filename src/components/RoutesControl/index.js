bc14b2440e2e341fcb86ba6169807a990d399827
Thanh Phong <tangthanh.phong99@gmail.com>
2023-05-14 09:15:27 +0700

    feat: check proTectedRoute on page

--- src/components/RoutesControl/index.js
+++ src/components/RoutesControl/index.js
@@ -1,5 +1,6 @@
 import ProtectedRoute from './ProtectedRoute'
-import HomeProtectedRoute from './HomeProtectedRoute'
 import PublicRoute from './PublicRoute'
 
-export { ProtectedRoute, HomeProtectedRoute, PublicRoute }
+export {
+  ProtectedRoute, PublicRoute
+}
